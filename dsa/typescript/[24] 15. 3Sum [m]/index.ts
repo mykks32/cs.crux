d07d1810464c0ac3d75cffcaf28739a89ae0329a
Shree Krishna Yadav <srikri5462ydv@gmail.com>
2026-04-16 20:45:08 +0545

    feat(dsa/ts): [24] 15. 3Sum [m]

--- dsa/typescript/[24] 15. 3Sum [m]/index.ts
+++ dsa/typescript/[24] 15. 3Sum [m]/index.ts
@@ -1,59 +1,93 @@
 class ThreeSum {
-    constructor() {
-    }
+	constructor() {
+	}
 
-    /**
-     *
-     *
-     * @param nums
-     */
-    threeSum(nums: number[]): number[][] {
-        // Two Pointers
-        nums.sort((a, b) => a - b);
-        let index: number = 0;
-        let left: number = index + 1;
-        let right: number = nums.length - 1;
+	/**
+	 * Finds all unique triplets in the array which gives the sum of zero.
+	 *
+	 *
+	 * @param {number[]} nums - Input array of integers
+	 * @returns {number[][]} - Array of triplets [a, b, c] such that a + b + c = 0
+	 *
+	 * @example
+	 * threeSum([-1, 0, 1, 2, -1, -4])
+	 * // Possible output: [[-1, 0, 1], [-1, -1, 2]] (may vary due to logic limitations)
+	 *
+	 * @timeComplexity O(n^2)
+	 *  - Sorting: O(n log n)
+	 *  - Outer loop: O(n)
+	 *  - Two-pointer scan: O(n) per iteration
+	 *
+	 * @spaceComplexity O(1)
+	 *  - Excluding output array
+	 */
+	public threeSum( nums: number[] ): number[][] {
+		// Sort the array
+		nums.sort(( a, b ) => a - b);
+		// First Index
+		let index: number = 0;
 
-        const output: Array<Array<number>> = [];
+		// Output Array
+		const output: Array<Array<number>> = [];
 
-        while (index < nums.length - 1) {
-            while (left < right) {
+		// Iterate to less than two index
+		while ( index < nums.length - 2 ) {
 
-                console.log('index', index, 'left', left, 'right', right);
-                const sum = nums[left] + nums[right];
-                if (sum == -nums[index]) {
-                    output.push([nums[index], nums[left], nums[right]]);
-                    console.log(output);
-                }
+			// Skip duplicate index
+			if ( index > 0 && nums[index] === nums[index - 1] ) {
+				index++;
+				continue;
+			}
+			// Two Pointer
+			let left = index + 1;
+			let right = nums.length - 1;
 
-                if (nums[left] < nums[right]) {
-                    left++
-                } else {
-                    right--;
-                }
-            }
-            index++;
-            left = index + 1;
-            right = nums.length - 1;
-        }
+			while ( left < right ) {
+				// Find Sum of two number and compare it with index element
+				const sum = nums[index] + nums[left] + nums[right];
+				if ( sum === 0 ) {
+					output.push([ nums[index], nums[left], nums[right] ]);
 
-        return output;
-    }
+					// if sum < index element, increment left otherwise decrement right
+					// Skip duplicate left
+					while ( left < right && nums[left] === nums[left + 1] ) {
+						left++
+					}
+
+					// Skip duplicate right
+					while ( left < right && nums[right] === nums[right - 1] ) {
+						right--;
+					}
+
+					left++;
+					right--;
+				} else if ( sum < 0 ) {
+					left++;
+				} else {
+					right--;
+				}
+			}
+			// Increase index
+			index++;
+		}
+
+		return output;
+	}
 }
 
 /**
  * Self-inducing Test Block
  */
-(() => {
-        // Create Instance
-        const
-            obj = new ThreeSum();
+( () => {
+		// Create Instance
+		const
+			obj = new ThreeSum();
 
-        // Test Case Block using object
-        console.log(obj.threeSum([-1, 0, 1, 2, -1, -4]));
-        // console.log(obj.threeSum([0, 1, 1]))
-        // console.log(obj.threeSum([0, 0, 0]))
-    }
+		// Test Case Block using object
+		console.log(obj.threeSum([ -1, 0, 1, 2, -1, -4 ]));
+		console.log(obj.threeSum([ 0, 1, 1 ]))
+		console.log(obj.threeSum([ 0, 0, 0 ]))
+	}
 
 )
 ()
